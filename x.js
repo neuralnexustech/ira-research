32e013b067a99fc63779b18eb24772f4c02f2ec7
shivaprasad <neuralnexustech.com@gmail.com>
2026-05-23 08:46:22 +0530

    Remove leftover x.js

--- x.js
+++
@@ -1,6 +0,0 @@
-// Prioritize environment variable (MCP config)
-if (process.env.IRA_EXTENSION_ID) {
-  extensionId = process.env.IRA_EXTENSION_ID;
-  console.error(`Using Extension ID from environment:.${process.env.IRA_EXTENSION_ID}`);
-  updateNativeManifest(extensionId);
-}
